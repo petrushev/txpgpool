1cac1c1857921ca232d5f89cd2ab7e2042d1ec23
petrushev <b.petrushev@gmail.com>
2014-08-31 04:08:44 +0200

    Major refactoring

--- txpgpool/__init__.py
+++ txpgpool/__init__.py
@@ -1,21 +1,23 @@
 from collections import deque
 
-from twisted.internet import reactor
-from twisted.internet.defer import Deferred
+from twisted.internet.defer import Deferred, succeed
 from twisted.python import log
 
 from psycopg2 import connect as pgConnect
-from psycopg2.extras import DictConnection as _DictConnection
+from psycopg2.extras import DictConnection
 from txpostgres.txpostgres import Connection
 
-class DictConnection(Connection):
+class TxDictConnection(Connection):
     @staticmethod
     def connectionFactory(*args, **kwargs):
-        kwargs['connection_factory'] = _DictConnection
+        kwargs['connection_factory'] = DictConnection
         return pgConnect(*args, **kwargs)
 
 def connect(**params):
-    return DictConnection().connect(**params)
+    return TxDictConnection().connect(**params)
+
+def echo(failure):
+    log.err(failure.getErrorMessage())
 
 class BasePgPool(object):
 
@@ -24,31 +26,40 @@ class BasePgPool(object):
     def runQuery(self, *args, **kwargs):
         """Returns a defered that will fire the callback with the query result when
         the latter is available. Internally it uses the `_connRunQuery` as an
-        intermediate callbackand and `_queryFinished` as a final one."""
-        d = self.fetch()
-        d.addCallback(self._connRunQuery, *args, **kwargs)
+        intermediate callback."""
+        d = Deferred()
+
+        internal = self.fetch()
+        internal.addCallback(self._connRunQuery, d, *args, **kwargs)
+        internal.addErrback(d.errback)
+
         return d
 
-    def _connRunQuery(self, conn, *args, **kwargs):
-        """Called imediatelly after a connection is ready
-        for the deferred from `runQuery`"""
-        d = conn.runQuery(*args, **kwargs)
-        # adds a callback _after_ the user callback is added
-        reactor.callLater(0, d.addCallback, self._queryFinished, d, conn)
-        return d
+    def _connRunQuery(self, conn, final, *args, **kwargs):
+        """Called when a connection is acquired for the deferred from`runQuery`.
+        The result will proceed to the callback, and the connection will be
+        returned to the pool"""
 
-    def _queryFinished(self, callbackResult, d, conn):
-        """This callback is fired after the user-set callback on the deferred from
-        `runQuery` is complete, and will be called with its result"""
-        self.putback(conn)
+        internal = conn.runQuery(*args, **kwargs)
+
+        @internal.addCallback
+        def onQueryResult(result):
+            self.putback(conn)
+            final.callback(result)
+
+        @internal.addErrback
+        def onQueryFailure(failure):
+            self.putback(conn)
+            final.errback(failure)
+
+        return final
 
     def addNotifyObserver(self, observerCallback, channels):
         """Sets async listening on `channels`. The payloads from all of them
         will be passed to `observerCallback`.
         Will hold one connection indefinetely"""
-        self.fetch()\
-            .addCallback(self._addConnObserver, observerCallback, channels)\
-            .addErrback(self._notifyError)
+        d = self.fetch()
+        d.addCallback(self._addConnObserver, observerCallback, channels)
 
     def _addConnObserver(self, conn, observerCallback, channels):
         conn.addNotifyObserver(observerCallback)
@@ -58,10 +69,6 @@ class BasePgPool(object):
 
             conn.runOperation('LISTEN ' + channel)\
                 .addErrback(self._notifyError)
-
-    def _notifyError(self, failure):
-        pass
-        #failure.printTraceback()
 
     def fetch(self):
         """Returns a deferred that will fire its callback with a connection when
@@ -73,151 +80,151 @@ class BasePgPool(object):
         """Call to put a connection back to the pool explicitly."""
         raise NotImplementedError
 
-    def destroyed(self):
-        """Call to notify the pool that a connection is closed outside its control"""
-        raise NotImplementedError
-
 
 class NullPgPool(BasePgPool):
-    """Does not do any pooling, provides direct connection to a single connection"""
+    """Does not do any pooling, provides direct connection to one single client at a time"""
 
     def __init__(self, **kwargs):
         self._params = kwargs
-        self.fetched = False
+        self.ready = True
         self._waitingForConn = deque()
 
     def fetch(self):
         d = Deferred()
-        self._waitingForConn.append(d)
 
-        if self.fetched == False:
-            self.fetched = True
-            connect(**self._params).addCallback(self._connReady)
+        if not self.ready:
+            self._waitingForConn.append(d)
+            return d
+
+        # available for connecting
+        self.ready = False
+
+        internal = connect(**self._params)
+        internal.addCallback(d.callback)
+
+        @internal.addErrback
+        def onConnError(failure):
+            self.ready = True
+            d.errback(failure)
 
         return d
-
-    def _connReady(self, conn):
-        if self._waitingForConn:
-            d = self._waitingForConn.popleft()
-            d.callback(conn)
-        else:
-            self.putback(conn)
 
     def putback(self, conn):
         conn.close()
-        if self._waitingForConn:
-            connect(**self._params).addCallback(self._connReady)
-        else:
-            self.fetched = False
+        self._next()
 
-    def destroyed(self):
-        self.fetched = False
+    def _next(self):
+        if self._waitingForConn:
+            d = self._waitingForConn.popleft()
+
+            internal = connect(**self._params)
+            internal.addCallback(d.callback)
+
+            @internal.addErrback
+            def onConnError(failure):
+                d.errback(failure)
+                self._next()
+
+        else:
+            self.ready = True
 
 class StaticPgPool(BasePgPool):
+    """Maintains only one connection, the clients must wait for it"""
 
     def __init__(self, **kwargs):
         self._params = kwargs
         self.conn = None
-        self.fetched = False
+        self.ready = True
         self._waitingForConn = deque()
 
     def fetch(self):
+        if self.conn is not None:
+            if self.ready:
+                # connected and available
+                self.ready = False
+                return succeed(self.conn)
+
+            # connected but busy
+            d = Deferred()
+            self._waitingForConn.append(d)
+            return d
+
+        # conn is None: first time connecting
+        if not self.ready:
+            raise ValueError, 'Programing error: first time connecting on busy conn.'
+
         d = Deferred()
 
-        if self.fetched or self.conn is None or self._waitingForConn:
-            self._waitingForConn.append(d)
-            if self.conn is None:
-                # connecting for the first time
-                connect(**self._params).addCallback(self._connReady)
-        else:
-            # available now
-            self.fetched = True
-            reactor.callLater(0, d.callback, self.conn)
+        internal = connect(**self._params)
+        internal.addErrback(d.errback)
+
+        @internal.addCallback
+        def onConnReady(conn):
+            self.ready = False
+            self.conn = conn
+            d.callback(conn)
 
         return d
 
-    def _connReady(self, conn):
-        self.conn = conn
-        if not self.fetched and self._waitingForConn:
-            self.fetched = True
+    def putback(self, conn):
+        if self._waitingForConn:
             d = self._waitingForConn.popleft()
             d.callback(conn)
 
-    def putback(self, conn):
-        self.fetched = False
-        self._connReady(conn)
+        else:
+            self.ready = True
 
-    def destroyed(self):
-        self.conn = None
-        self.fetched = False
-        if self._waitingForConn:
-            connect(**self._params).addCallback(self._connReady)
-
-class QueuePgPool(set, BasePgPool):
-    """MinMax connection pool. Tries to maintain `min` number of ready connections
-    - if there are more clients will connect on demand but won't exceed `max` """
+class QueuePgPool(BasePgPool):
+    """MinMax connection pool. Tries to maintain `min` number of ready connections.
+    If there are more clients will connect on demand but won't exceed `max` """
 
     def __init__(self, minConn, maxConn, **kwargs):
         self._params = kwargs
         self._min = minConn
         self._max = maxConn
-        self._currentTotal = 0
+        self._available = set()
+        self._numBusy = 0
         self._waitingForConn = deque()
-        set.__init__(self, [])
-
-        reactor.callLater(0, self._poolReady)
-
-    def _poolReady(self):
-        #log.msg('ready:%d, total:%d, waiters:%d' % \
-        #    (len(self), self._currentTotal, len(self._waitingForConn)))
-        while len(self) > 0 and self._waitingForConn:
-            conn = self.pop()
-            reactor.callLater(0, self._connReady, conn)
-
-        if self._currentTotal < self._max and len(self) < self._min:
-            self._currentTotal = self._currentTotal + 1
-            connect(**self._params).addCallback(self._connReady)
-
-    def _connReady(self, conn):
-        """Called when a connection is ready, either by connecting
-        or when a client puts it back"""
-        if self._waitingForConn:
-            d = self._waitingForConn.popleft()
-            d.callback(conn)
-
-        elif len(self) == self._min:
-            # pool has enough ready connections, this one is closed
-            conn.close()
-            self._currentTotal = self._currentTotal - 1
-
-        else:
-            self.add(conn)
-
-        self._poolReady()
 
     def fetch(self):
+        if len(self._available) > 0:
+            conn = self._available.pop()
+            self._numBusy = self._numBusy + 1
+            return defer.succeed(conn)
+
+        # no available connections
         d = Deferred()
-        self._waitingForConn.append(d)
 
-        if len(self) > 0:
-            conn = self.pop()
-            reactor.callLater(0, self._connReady, conn)
+        if self._numBusy == self._max:
+            # overloaded
+            self._waitingForConn.append(d)
+            return d
 
-        elif self._currentTotal < self._max:
-            self._currentTotal = self._currentTotal + 1
-            connect(**self._params).addCallback(self._connReady)
+        # connections busy but can make new one
+        self._numBusy = self._numBusy + 1
+
+        internal = connect(**self._params)
+        internal.addCallback(d.callback)
+
+        @internal.addErrback
+        def onConnError(failure):
+            self._numBusy = self._numBusy - 1
+            d.errback(failure)
 
         return d
 
     def putback(self, conn):
-        if len(self) == self._min:
-            conn.close()
-            self._currentTotal = self._currentTotal - 1
+        if self._waitingForConn:
+            d = self._waitingForConn.popleft()
+            d.callback(conn)
+
         else:
-            self.add(conn)
+            self._numBusy = self._numBusy - 1
 
-        self._poolReady()
+            if len(self._available) == self._min:
+                # min ready connections satisfied
+                # close this one as excessive
+                conn.close()
 
-    def destroyed(self):
-        self._currentTotal = self._currentTotal - 1
-        self._poolReady()
+            else:
+                self._available.add(conn)
